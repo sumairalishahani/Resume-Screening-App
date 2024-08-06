e6510a16e221e06fa7865f90f5d5b6bc2e2bf42c
Sumair Ali <sumairalishahani1@gmail.com>
2024-08-06 21:55:01 +0500

    first commit

--- app.py
+++ app.py
@@ -1,36 +1,46 @@
 import streamlit as st
 import pickle
 import re
-import nltk
+import fitz  # PyMuPDF
 
-nltk.download('punkt')
-nltk.download('stopwords')
 
-#loading models
-clf = pickle.load(open('clf.pkl','rb'))
-tfidfd = pickle.load(open('tfidf.pkl','rb'))
+# Loading models
+clf = pickle.load(open('clf.pkl', 'rb'))
+tfidfd = pickle.load(open('tfidf.pkl', 'rb'))
 
-def clean_resume(resume_text):
-    clean_text = re.sub('http\S+\s*', ' ', resume_text)
-    clean_text = re.sub('RT|cc', ' ', clean_text)
-    clean_text = re.sub('#\S+', '', clean_text)
-    clean_text = re.sub('@\S+', '  ', clean_text)
-    clean_text = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', clean_text)
-    clean_text = re.sub(r'[^\x00-\x7f]', r' ', clean_text)
-    clean_text = re.sub('\s+', ' ', clean_text)
-    return clean_text
-# web app
+def clean_resume(txt):
+    clean_txt = re.sub(r'http\S+\s', ' ', txt)
+    clean_txt = re.sub(r'RT|cc', ' ', clean_txt)
+    clean_txt = re.sub(r'@\S+', ' ', clean_txt)
+    clean_txt = re.sub(r'#\S+\s', ' ', clean_txt)
+    clean_txt = re.sub(r'[%s]' % re.escape(r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', clean_txt)
+    clean_txt = re.sub(r'[^\x00-\x7f]', ' ', clean_txt)
+    clean_txt = re.sub(r'\s+', ' ', clean_txt)
+    return clean_txt
+
+def extract_text_from_pdf(pdf_file):
+    document = fitz.open(stream=pdf_file, filetype="pdf")
+    text = ""
+    for page_num in range(len(document)):
+        page = document.load_page(page_num)
+        text += page.get_text()
+    return text
+
+# Web app
 def main():
     st.title("Resume Screening App")
-    uploaded_file = st.file_uploader('Upload Resume', type=['txt','pdf'])
+    uploaded_file = st.file_uploader('Upload Resume', type=['txt', 'pdf'])
 
     if uploaded_file is not None:
-        try:
-            resume_bytes = uploaded_file.read()
-            resume_text = resume_bytes.decode('utf-8')
-        except UnicodeDecodeError:
-            # If UTF-8 decoding fails, try decoding with 'latin-1'
-            resume_text = resume_bytes.decode('latin-1')
+        if uploaded_file.type == 'application/pdf':
+            resume_text = extract_text_from_pdf(uploaded_file.read())
+        else:
+            try:
+                resume_bytes = uploaded_file.read()
+                resume_text = resume_bytes.decode('utf-8')
+            except UnicodeDecodeError:
+                # If UTF-8 decoding fails, try decoding with 'latin-1'
+                resume_text = resume_bytes.decode('latin-1')
 
         cleaned_resume = clean_resume(resume_text)
         input_features = tfidfd.transform([cleaned_resume])
@@ -67,11 +77,8 @@ def main():
         }
 
         category_name = category_mapping.get(prediction_id, "Unknown")
-
         st.write("Predicted Category:", category_name)
 
-
-
-# python main
+# Python main
 if __name__ == "__main__":
     main()
